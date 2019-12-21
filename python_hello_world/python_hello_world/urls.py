59ff858ea4dc79dac7b344a3112367023599f5fc
Xiao <petsgre@gmail.com>
2019-12-21 17:32:52 +0800

    refactor:modify the project name

--- python_hello_world/python_hello_world/urls.py
+++ python_hello_world/python_hello_world/urls.py
@@ -1,4 +1,4 @@
-"""Python_zx URL Configuration
+"""python_hello_world URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
--- python_hello_world/python_hello_world/wsgi.py
+++ python_hello_world/python_hello_world/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Python_zx project.
+WSGI config for python_hello_world project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Python_zx.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'python_hello_world.settings')
 
 application = get_wsgi_application()
