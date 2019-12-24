0198c9e306a6460dd69ff5d33a9c86a7bd5afe08
Xiao <petsgre@gmail.com>
2019-12-24 17:26:00 +0800

    feat:add drf

--- python_hello_world/urls.py
+++ python_hello_world/urls.py
@@ -17,8 +17,19 @@ from django.contrib import admin
 from django.urls import path
 from . import view
 
+from django.conf.urls import url, include
+from rest_framework import routers
+from python_hello_world.quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', view.hello),
     path('index/', view.index),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
